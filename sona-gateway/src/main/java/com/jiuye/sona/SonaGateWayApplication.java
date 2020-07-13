0192e1c591f382e8a2745843533a2961df1cabb0
unknown <huxinjian@sh-jiuye.com.cn>
2020-07-13 17:21:10 +0800

    #SONA集成服务网关

--- sona-gateway/src/main/java/com/jiuye/sona/SonaGateWayApplication.java
+++ sona-gateway/src/main/java/com/jiuye/sona/SonaGateWayApplication.java
@@ -1,13 +1,22 @@
 package com.jiuye.sona;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@Slf4j
 @SpringBootApplication
+@EnableEurekaClient
 public class SonaGateWayApplication {
 
+    /**
+     * 服务网关
+     * @param args
+     */
     public static void main(String[] args) {
         SpringApplication.run(SonaGateWayApplication.class, args);
+        log.info("**********sona-gateway 启动成功************");
     }
 
 }
